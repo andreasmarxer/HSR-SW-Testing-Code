1c0fe648eaf2ff5be78f849c599ca1a050b64b40
Andreas Marxer <andreas.marxer@hotmail.com>
2023-10-14 13:51:46 +0200

    exercise 3.4 testing with parametrized data input

---
+++ src/test/java/ch/hsr/testing/unittest/parametrized/PrimeCheckerTest.java
@@ -0,0 +1,35 @@
+package ch.hsr.testing.unittest.parametrized;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PrimeCheckerTest {
+
+	@Test
+	@MethodSource("createPrimeTestInput")
+	void testPrimeCheckerParametrized(int input, boolean isPrime) {
+		boolean result = PrimeChecker.isPrime(input);
+		MatcherAssert.assertThat(result, Matchers.is(isPrime));
+	}
+	
+	private static Stream<Arguments> createPrimeTestInput() {
+        return Stream.of(
+                Arguments.of(1, true),
+                Arguments.of(2, true),
+                Arguments.of(3, true),
+                Arguments.of(4, false),
+                Arguments.of(9, false),
+                Arguments.of(22, false),
+                Arguments.of(23, true),
+                Arguments.of(144, false),
+                Arguments.of(7187, true)
+        );
+    }
+
+}
