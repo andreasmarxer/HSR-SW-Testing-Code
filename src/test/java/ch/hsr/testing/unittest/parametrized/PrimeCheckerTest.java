1cd07d79971ff187ed6a016eb3c67f08035f573c
Andreas Marxer <andreas.marxer@hotmail.com>
2023-10-14 16:22:59 +0200

    fixed issue in exercise 3.4 of missing import

--- src/test/java/ch/hsr/testing/unittest/parametrized/PrimeCheckerTest.java
+++ src/test/java/ch/hsr/testing/unittest/parametrized/PrimeCheckerTest.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class PrimeCheckerTest {
